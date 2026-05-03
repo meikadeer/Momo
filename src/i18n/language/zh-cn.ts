164de6eee5f3a8cb4cb10968453b2d9e1a93cac9
meikadeer <102945149+meikadeer@users.noreply.github.com>
2026-05-03 08:28:41 +0800

    Update zh-cn.ts

--- src/i18n/language/zh-cn.ts
+++ src/i18n/language/zh-cn.ts
@@ -9,15 +9,15 @@ const translation: Translation = {
     },
     cover: {
         title: {
-            home: "欢迎来到Momo的博客",
+            home: "欢迎来到Quan的博客",
             archive: "文章归档",
             about: "关于",
             friends: "友链",
         },
         subTitle: {
-            home: "生活多彩！",
+            home: "从乱麻中寻回秩序，在庸常里打捞不同",
             archive: "共 {count} 篇文章",
-            about: "一个极简的Blog模板",
+            about: "一个脱离了低级趣味的人",
             friends: "有趣的灵魂",
         }
     },
